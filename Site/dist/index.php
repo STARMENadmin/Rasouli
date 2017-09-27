baa8735dd090da0fb491443a0559f949b85c8564
Kaz Alvis <info@superkore.com>
2017-09-27 12:32:17 -0700

    Homepage

--- Site/dist/index.php
+++ Site/dist/index.php
@@ -19,14 +19,14 @@
 		  
 			<div id="home-hero-copy-2">
 				<div class="left">
-					<p>About our practice</p>
+					<p class="smallTitle">About our practice</p>
 					<p>Tomorrow <span>is here</span></p>
 				</div>
 				<div class="right">
 					
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui. In sollicitudin orci purus, in sollicitudin quam finibus tincidunt. Vivamus dapibus lectus sit amet lectus accumsan, at posuere risus gravida. Mauris id ante in urna pellentesque blandit. In eget tincidunt nisi, sit amet aliquet eros.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui. In sollicitudin orci purus, in sollicitudin quam finibus tincidunt. Vivamus dapibus lectus sit amet lectus accumsan, at posuere risus gravida. </p>
 					
-					<a href="#">Learn More<span></span></a>
+					<a class="textCTA white" href="#">Learn More<span class="linkArrow"></span></a>
 					
 				</div>    
 			</div>
@@ -34,19 +34,34 @@
 	     <img class="hero-background" src="img/home-hero.jpg"> 
       </section>
       
-      <!-- Hero Section -->
-		<section id="home-procedures" class="home-section-2">
+      <!-- Procedures and Conditions Section -->
+		<section id="home-procedures" class="home-section-2 group">
 			
 			<div class="top">
-				<h2>Procedures and Conditions</h2>
+				<p class="smallTitle">Procedures and Conditions</p>
 				
-				<h3>We've Got Your Back</h3>
+				<p class="largeTitle">We've Got Your Back</p>
 				
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui.</p>
+				<p class="copy centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui.</p>
 				
-				<p>Select image below to see images and procedures.</p>
+				<p class="textCTA">Select image below to see images and procedures.</p>
 			<div>
 			
+		</section>
+		
+		<!-- The Surgeon Section -->
+		<section id="home-Surgeon" class="home-section-4 group twoColumn">
+			
+			<div class="left">
+				<img src="img/theSurgen.jpg"/>
+			</div>
+			<div class="right">
+				<p class="smallTitle">The Surgen</p>
+				<p class="largeTitle">Dr. Alexandre Rasouli</p>
+				<p class="copy ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui.</p>
+				
+			</div>
+			<div class="clear"></div>
 		</section>
       
          <?php include("partials/footer.php"); ?>
