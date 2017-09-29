7be189a0b5f4199f2fe7f9fa19aafa240af0590f
Kaz Alvis <info@superkore.com>
2017-09-29 15:32:09 -0700

    Footer

--- Site/dist/partials/footer.php
+++ Site/dist/partials/footer.php
@@ -28,7 +28,43 @@
 			 </ul>
 			 <div class="clear"></div>
 	  	</nav>
-	</div>
+        
+        <div class="footerBottom">
+            <div class="left">
+                <div class="logoWrapper">
+                    <img id="absLogo" src="img/absLogo.png"/>
+                </div>
+                <div class="logoWrapper">
+                    <img id ="hipaaLogo"src="img/hipaaLogo.png"/>
+                </div> 
+                <div class="logoWrapper">
+                    <img id="abnsLogo" src="img/abnsLogo.png"/>
+                </div>    
+                <div class="clear"></div>
+            </div>
+            <div class="center">
+                <div class="newsletter-wrapper">
+                        <form action="mail.php" method="POST">
+                            <input type="text" name="email" placeholder="Sign up for newsletter">
+                            <input class="newsletterSubmit" type="image" src="img/newsletterArrow.png" border="0" alt="Submit">
+                        </form>
+                    <div class="clear"></div>
+                </div>
+            
+                <p>Copyright 2017 Rasouli Spine | Making Brands Work</p>
+            </div><!-- center -->
+            <div class="right">
+                <div class="socialWrapper">
+                    <div class="socialIcon twitter"></div>
+                    <div class="socialIcon facebook"></div>
+                    <div class="socialIcon instagram"></div>
+                    <div class="socialIcon youtube"></div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div><!-- footerBottom -->
+        
+	</div><!-- footer inner-->
 	 
 </footer>
  
