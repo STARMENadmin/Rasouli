ecab33e02cce8558cbefff5efaf36414dad2cadf
Kaz Alvis <info@superkore.com>
2017-10-23 13:39:23 -0700

    2nd top nav

--- Site/dist/index.php
+++ Site/dist/index.php
@@ -2,6 +2,7 @@
  
     <body class="hompage">
 	     <?php include("partials/nav.php"); ?>
+	      <?php include("partials/nav-static.php"); ?>
 
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
