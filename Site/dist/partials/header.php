c7a86913c1af1fba0e4e2305ed95f3711392ca34
Kaz Alvis <info@superkore.com>
2017-11-13 17:58:12 -0800

    header

--- Site/dist/partials/header.php
+++ Site/dist/partials/header.php
@@ -46,7 +46,7 @@ if (stripos($_SERVER['REQUEST_URI'], 'patient-resources.php')){
 ?>
 
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
