d6cb424f44f3020b7f836baea0f1a95c3fbb3e06
Kaz Alvis <info@superkore.com>
2017-11-15 15:25:14 -0800

    css

--- Site/dist/partials/outpatient-hero.php
+++ Site/dist/partials/outpatient-hero.php
@@ -1,8 +1,10 @@
 
               <picture>
-                <source media="(max-width: 768px)" srcset="img/outpatientHero-mobile.jpg">
-                 <img alt="outpatient Hero" class="hero-background" src="img/outpatientHero.jpg"> 
-            </picture> 	
+                <source  srcset="img/outpatientHero-mobile.jpg" media="(max-width: 768px)">
+                 <source  srcset="img/outpatientHero.jpg">
+                 <img alt="outpatient Hero" class="hero-background" src="img/outpatientHero.jpg"/> 
+            </picture> 
+            	
   	    	
 			<div id="home-hero-copy-1" class="outpatient-hero-copy-1">
 		      <h1>Modern Options<span class="block">Outside the Hospital</span></h1>
