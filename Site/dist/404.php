291dd66876c58b9c56c1989b013fe5102af839b4
Kaz Alvis <info@superkore.com>
2017-12-01 10:36:17 -0800

    404 page

--- Site/dist/404.php
+++ Site/dist/404.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
  <?php include("partials/header.php"); ?>
 <body>
 
@@ -9,6 +10,26 @@
     <img src="img/404image.png"/>
     <h1>WE CAN’T SEEM TO FIND <br/>THE PAGE YOU’RE LOOKING FOR</h1>
      <p>HERE ARE SOME<br/>HELPFUL LINKS INSTEAD:</p>
+=======
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Page Not Found</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+             <link type='text/css' rel="stylesheet" href="/css/main.css">
+
+     
+
+</head>
+<body>
+
+    <h1>404</h1>
+    <p>WE CAN’T SEEM TO FIND <br/>THE PAGE YOU’RE LOOKING FOR</p>
+     <p>HERE ARE SOME: <br/>HELPFUL LINKS INSTEAD:</p>
+>>>>>>> Stashed changes
      
 		<nav class="error-nav">
 		 				<ul>
