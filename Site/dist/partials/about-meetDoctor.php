b3ad501ce874aa63b82ae1fd9f5299dc1ba84afd
jaystarmen <34355761+jaystarmen@users.noreply.github.com>
2018-01-09 15:06:54 -0800

    Update about-meetDoctor.php

--- Site/dist/partials/about-meetDoctor.php
+++ Site/dist/partials/about-meetDoctor.php
@@ -3,14 +3,13 @@
                 <source media="(max-width: 768px)" srcset="img/theSurgenMobile.jpg">
                 <img alt="about doctor image" src="img/theSurgen.jpg" >
                 </picture>
-                <p class="imageCopy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan.</p>
 			</div>
 			<div class="right parallax" data-parallax='{"y" : 10, "smoothness": 10}'>
 				<p class="smallTitle">Meet The Doctor</p>
 				<p class="largeTitle">Dr. Alexandre <span class="block">Rasouli</span></p>
-				<p class="copy ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui.</p>
-                <p class="copy ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque faucibus a nisl et accumsan. Maecenas rhoncus quam non ipsum dapibus dignissim. Pellentesque tortor nunc, molestie sit amet ipsum eu, commodo lacinia dui.</p>
-				
+				<p class="copy ">Alexandre Rasouli, MD is a nationally-recognized, board-certified spine surgeon who specializes in the surgical and nonsurgical management of cervical, thoracic and lumbar disorders of the spine, degenerative disorders, spinal trauma, adult and pediatric deformity, spinal oncology and minimally invasive surgery.</p>
+                <p class="copy ">Dr. Rasouli gained prominence in the field of spinal disorders after his 2007 discovery of a novel method of regenerating neurons damaged by spinal cord injury using peripheral nerve transplantation. A Stanford honors graduate with extensive training in neurosurgical and orthopedic surgery techniques, he was also the first to describe the genetic response of spinal cord tissue to traumatic injury. His research focuses on developing a cure for paralysis.</p>
+				<p class="copy ">He has authored over 50 manuscripts, abstracts, book chapters and presentations in neurological and orthopedic literature. He leads many clinical trials for advanced surgical techniques, including motion-preserving and minimally-invasive surgery. His practice attracts patients from around the world.</p>
 				
 				
 			</div>
